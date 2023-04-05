a9e4039849d4c511adad5d1fd68db94b717e4d13
mattpr1c3 <matthew.pr1c3@gmail.com>
2023-04-05 13:48:37 -0700

    set up the fetch and state and pokemon list

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import AllPokemon from "./components/AllPokemon";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [pokemonList, setPokemonList] = useState([]);
+
+  function apiCall() {
+    fetch(
+      `https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data.pokemon);
+        setPokemonList(data.pokemon);
+      })
+
+      .catch((err) => console.error(err));
+  }
+  apiCall();
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      <AllPokemon />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
